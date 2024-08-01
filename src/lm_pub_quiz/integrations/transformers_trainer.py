d219d44ef4ca3aa4b9a2c8048c5ad5fc34a57996
Sebastian Pohl <sebpohl@gmx.net>
2024-08-01 16:04:01 +0200

    initialize trainer with evaluator and dataset

--- src/lm_pub_quiz/integrations/transformers_trainer.py
+++ src/lm_pub_quiz/integrations/transformers_trainer.py
@@ -1,32 +1,19 @@
 import numpy as np
 from transformers import TrainerCallback
 
-from lm_pub_quiz.data import Dataset
-from lm_pub_quiz.evaluator import Evaluator
-
 
 class PubQuizCallback(TrainerCallback):
     """This callback handels on the fly BEAR evaluation for the huggingface Trainer class"""
-    def __init__(self, trainer, dataset_path, metrics=("accuracy"), model_type=None, save_path=None, batch_size=32, template=0):
-        self.trainer = trainer
-        self.model_type = model_type
+    def __init__(self, evaluator, dataset, metrics=None, save_path=None, batch_size=32, template=0):
+        self.evaluator = evaluator
+        self.dataset = dataset
         self.save_path = save_path
         self.batch_size = batch_size
         self.template = template
-        if "accuracy" in metrics and "num_instances" not in metrics:
-            metrics = metrics + ("num_instances",)
         self.metrics = metrics
-        # include default loading of dataset from url?
-        self.dataset = Dataset.from_path(dataset_path)
 
     def on_evaluate(self, args, state, control, **kwargs):
-        evaluator = Evaluator.from_model(
-            kwargs["model"],
-            model_type=self.model_type,
-            tokenizer=kwargs["tokenizer"],
-            device=kwargs["model"].device,
-        )
-        result = evaluator.evaluate_dataset(self.dataset, template_index=self.template, batch_size=self.batch_size)
+        result = self.evaluator.evaluate_dataset(self.dataset, template_index=self.template, batch_size=self.batch_size)
         if self.save_path:
             result.save(f"{self.save_path}/{state.epoch}_bear_results")
 
