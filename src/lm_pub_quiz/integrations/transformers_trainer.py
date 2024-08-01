f9fc3292220e53c516fc3b094f59fbdce19327ba
Sebastian Pohl <sebpohl@gmx.net>
2024-08-01 17:43:28 +0200

    add metrics support to Callback

--- src/lm_pub_quiz/integrations/transformers_trainer.py
+++ src/lm_pub_quiz/integrations/transformers_trainer.py
@@ -11,7 +11,7 @@ class PubQuizCallback(TrainerCallback):
             trainer: required to call trainer logging functionality
             evaluator:
             dataset:
-            metrics: one of [None, "overall", "domains", "cardinalities"]
+            metrics: one of [None, "overall", "domains", "cardinality"]
             save_path: if specified, complete BEAR results for each evaluation are saved under the path
             batch_size:
             template: specify list of or single BEAR template indices to be used for evaluation
@@ -24,19 +24,38 @@ class PubQuizCallback(TrainerCallback):
         self.template = template
         self.metrics = metrics
 
-    def on_evaluate(self, args, state, control, **kwargs):
-
-        result = self.evaluator.evaluate_dataset(self.dataset, template_index=self.template, batch_size=self.batch_size)
-        if self.save_path:
-            result.save(f"{self.save_path}/{state.epoch}_bear_results")
-
-        overall_score = result.get_metrics(["accuracy", "support"], accumulate=True)
-        metrics_data = {
-            "eval_bear_score": overall_score
-        }
+        # check dataset for compatibility with requested metrics
         if self.metrics == "domains":
-            domain_scores = result.get_metrics(["accuracy", "support"], accumulate="domains")
-        elif self.metrics == "cardinality":
-            cardinality_scores = result.get_metrics(["accuracy", "support"], accumulate="cardinality")
+            try:
+                dataset[0].relation_info("domains")
+            except KeyError as e:
+                raise ValueError(f"Cannot retrieve domain results without domain info in dataset.")
+
+    def on_evaluate(self, args, state, control, **kwargs):
+        if isinstance(self.template, int):
+            templates = [self.template]
+            flag_template = False
+        else:
+            templates = self.template
+            flag_template = True
+
+        metrics_data = dict()
+        for template in templates:
+            template_suffix = "_" + str(template) if flag_template else ""
+            result = self.evaluator.evaluate_dataset(self.dataset, template_index=self.template, batch_size=self.batch_size)
+            if self.save_path:
+                result.save(f"{self.save_path}/{state.epoch}_bear_results{template_suffix}")
+
+            overall_score = result.get_metrics(["accuracy", "support"], accumulate=True)
+            metrics_data[f"eval_bear_score{template_suffix}"] = overall_score["accuracy"]
+
+            if self.metrics == "domains":
+                domain_scores = result.get_metrics(["accuracy", "support"], accumulate="domains")
+                for domain, score in domain_scores["accuracy"].items():
+                    metrics_data[f"eval_bear_{domain}{template_suffix}"] = score
+            elif self.metrics == "cardinality":
+                cardinality_scores = result.get_metrics(["accuracy", "support"], accumulate="cardinality")
+                for cardinality, score in cardinality_scores["accuracy"].items():
+                    metrics_data[f"eval_bear_{cardinality.replace(' ', '_')}{template_suffix}"] = score
 
         self.trainer.log(metrics_data)
