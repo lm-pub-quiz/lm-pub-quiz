fddf733a8411f18b1e50fc91bd79ffde02b4ae01
Sebastian Pohl <sebpohl@gmx.net>
2024-08-01 17:05:13 +0200

    add metrics support to Callback

--- src/lm_pub_quiz/integrations/transformers_trainer.py
+++ src/lm_pub_quiz/integrations/transformers_trainer.py
@@ -4,7 +4,19 @@ from transformers import TrainerCallback
 
 class PubQuizCallback(TrainerCallback):
     """This callback handels on the fly BEAR evaluation for the huggingface Trainer class"""
-    def __init__(self, evaluator, dataset, metrics=None, save_path=None, batch_size=32, template=0):
+    def __init__(self, trainer, evaluator, dataset, metrics=None, save_path=None, batch_size=32, template=0):
+        """
+
+        Args:
+            trainer: required to call trainer logging functionality
+            evaluator:
+            dataset:
+            metrics: one of [None, "overall", "domains", "cardinalities"]
+            save_path: if specified, complete BEAR results for each evaluation are saved under the path
+            batch_size:
+            template: specify list of or single BEAR template indices to be used for evaluation
+        """
+        self.trainer = trainer
         self.evaluator = evaluator
         self.dataset = dataset
         self.save_path = save_path
@@ -13,16 +25,18 @@ class PubQuizCallback(TrainerCallback):
         self.metrics = metrics
 
     def on_evaluate(self, args, state, control, **kwargs):
+
         result = self.evaluator.evaluate_dataset(self.dataset, template_index=self.template, batch_size=self.batch_size)
         if self.save_path:
             result.save(f"{self.save_path}/{state.epoch}_bear_results")
 
-        bear_metrics = result.get_metrics(self.metrics)
-        weighted_accuracy = np.average(bear_metrics.accuracy, weights=bear_metrics.num_instances)
-
-        # TODO: Include additional metrics
+        overall_score = result.get_metrics(["accuracy", "support"], accumulate=True)
         metrics_data = {
-            "eval_bear_score": weighted_accuracy
+            "eval_bear_score": overall_score
         }
+        if self.metrics == "domains":
+            domain_scores = result.get_metrics(["accuracy", "support"], accumulate="domains")
+        elif self.metrics == "cardinality":
+            cardinality_scores = result.get_metrics(["accuracy", "support"], accumulate="cardinality")
 
         self.trainer.log(metrics_data)
