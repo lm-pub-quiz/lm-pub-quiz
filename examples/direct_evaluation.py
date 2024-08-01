44ccf9b2ab20573fd8290d67d21351053d79116f
Max Ploner <plonerma@maxploner.de>
2024-08-01 16:32:09 +0200

    Added a bit more context to the example script

--- examples/direct_evaluation.py
+++ examples/direct_evaluation.py
@@ -1,3 +1,5 @@
+"""This script illustrate how to load the dataset and perform an evaluation on a model."""
+
 from lm_pub_quiz import Dataset, Evaluator
 
 # Load the dataset
@@ -11,6 +13,7 @@ evaluator = Evaluator.from_model(
 # Run the evaluation and save the
 evaluator.evaluate_dataset(
     dataset,
+    template_index=0,
     save_path="gp2_results",
     batch_size=32,
 )
