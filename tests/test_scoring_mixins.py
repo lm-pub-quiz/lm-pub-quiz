45e7a2dc1c6080664b30b8d7b596eed9a4c2477c
Muziwahuzie <yggdrasilprecision@outlook.de>
2025-04-07 13:31:33 +0200

    Add test for sentence_l2r scoring

--- tests/test_scoring_mixins.py
+++ tests/test_scoring_mixins.py
@@ -2,6 +2,36 @@ import pytest
 
 from lm_pub_quiz.evaluators.scoring_mixins import MaskedLMScoringMixin
 
+def test_sentence_l2r(distilbert):
+    model, tokenizer = distilbert
+
+    scorer = MaskedLMScoringMixin.from_model("distilbert-base-cased", pll_metric="sentence_l2r")
+
+    batch = scorer.tokenizer(
+        ["The traveler lost the souvenir."],
+        return_tensors="pt",
+        padding=True,
+        return_special_tokens_mask=True,
+        return_length=True,
+    )
+
+    scores = scorer.score_statements(batch)[0]
+
+    # TODO: add test for correct masking here
+    reference_scores = [
+            -7.889340400695801,
+            -11.87872314453125,
+            -4.540374279022217,
+            -8.895203590393066,
+            -3.4925360679626465,
+            -7.935790538787842,
+            -2.4408531188964844,
+            0, #-5.364403477869928e-06
+            -1.5065549612045288
+    ]
+
+    for a, b in zip(scores, reference_scores):
+        assert a == pytest.approx(b, abs=1e-5)
 
 def test_within_word_l2r(distilbert):
     model, tokenizer = distilbert
